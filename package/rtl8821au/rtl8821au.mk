76111d502f7b41fafb4d9bc7dd03b9361bf07d58
Christian Stewart <christian@paral.in>
2015-08-23 20:35:01 -0400

    package/rtl8821au: change upstream to more recent fork
    The current rtl8821au driver maintained by ulli-kroll is based on a
    much older upstream version. I have forked the asus rtl8821au driver
    from http://support.asus.com/ and patched it with various fixes to
    improve stability and fix various bugs with kernel panics, newer kernel
    versions, and wps issues.
    
    With the newer version of the driver, we have not experienced any
    kernel panics, or connectivity issues. Power management works fine on
    or off.
    
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -4,22 +4,14 @@
 #
 ################################################################################
 
-RTL8821AU_VERSION = fef8c9d67d076eee9dfb9e19cb7cb69815d5a207
-RTL8821AU_SITE = $(call github,ulli-kroll,rtl8821au,$(RTL8821AU_VERSION))
-RTL8821AU_LICENSE = GPLv2, proprietary (rtl8821au.bin firmware)
+RTL8821AU_VERSION = 83b1e07b9ab5a98f6585068de5a13ce9646aca9a
+RTL8821AU_SITE = $(call github,paralin,rtl8821au,$(RTL8821AU_VERSION))
+RTL8821AU_LICENSE = GPLv2
 RTL8821AU_LICENSE_FILES = COPYING
 
 RTL8821AU_MODULE_MAKE_OPTS = \
-	CONFIG_RTL8812AU_8821AU=m \
-	KVER=$(LINUX_VERSION_PROBED) \
+	CONFIG_RTL8821AU=m \
 	USER_EXTRA_CFLAGS=-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN
-
-define RTL8821AU_FIRMWARE_INSTALL
-	mkdir -p $(TARGET_DIR)/lib/firmware/rtlwifi/
-	$(INSTALL) -D -m 0644 $(@D)/firmware/* $(TARGET_DIR)/lib/firmware/rtlwifi/
-endef
-
-RTL8821AU_POST_INSTALL_TARGET_HOOKS += RTL8821AU_FIRMWARE_INSTALL
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
