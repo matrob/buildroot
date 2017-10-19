3873fdf90c4900fba2b27097eecdaadb73816ce3
Christian Stewart <christian@paral.in>
2017-10-19 11:56:56 -0400

    docker-containerd: use golang-package infrastructure
    Signed-off-by: Christian Stewart <christian@paral.in>

--- package/docker-containerd/docker-containerd.mk
+++ package/docker-containerd/docker-containerd.mk
@@ -9,8 +9,6 @@ DOCKER_CONTAINERD_SITE = $(call github,containerd,containerd,$(DOCKER_CONTAINERD
 DOCKER_CONTAINERD_LICENSE = Apache-2.0
 DOCKER_CONTAINERD_LICENSE_FILES = LICENSE.code
 
-DOCKER_CONTAINERD_DEPENDENCIES = host-go
-
 DOCKER_CONTAINERD_GOPATH = "$(@D)/vendor"
 DOCKER_CONTAINERD_MAKE_ENV = $(HOST_GO_TARGET_ENV) \
 	CGO_ENABLED=1 \
@@ -45,4 +43,4 @@ define DOCKER_CONTAINERD_INSTALL_TARGET_CMDS
 	ln -fs containerd-shim $(TARGET_DIR)/usr/bin/docker-containerd-shim
 endef
 
-$(eval $(generic-package))
+$(eval $(golang-package))
